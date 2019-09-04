283824ce09ea16cca662fc86f4a735b6a8cf2278
PeterBot <PeterBot@users.noreply.github.com>
2019-09-05 03:17:52 +0800

    Add oojs-ui v0.34.0

---
+++ ajax/libs/oojs-ui/0.34.0/oojs-ui-apex.js
@@ -0,0 +1,45 @@
+/*!
+ * OOUI v0.34.0-pre (d5e74518ab)
+ * https://www.mediawiki.org/wiki/OOUI
+ *
+ * Copyright 2011–2019 OOUI Team and other contributors.
+ * Released under the MIT license
+ * http://oojs.mit-license.org
+ *
+ * Date: 2019-09-04T18:28:52Z
+ */
+( function ( OO ) {
+
+'use strict';
+
+/**
+ * @class
+ * @extends OO.ui.Theme
+ *
+ * @constructor
+ */
+OO.ui.ApexTheme = function OoUiApexTheme() {
+	// Parent constructor
+	OO.ui.ApexTheme.parent.call( this );
+};
+
+/* Setup */
+
+OO.inheritClass( OO.ui.ApexTheme, OO.ui.Theme );
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+OO.ui.ApexTheme.prototype.getDialogTransitionDuration = function () {
+	return 250;
+};
+
+/* Instantiation */
+
+OO.ui.theme = new OO.ui.ApexTheme();
+
+}( OO ) );
+
+//# sourceMappingURL=oojs-ui-apex.js.map.json
