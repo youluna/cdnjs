a21824fc9a568f8c8d2e599266b049bd268b0adc
robocdnjs <cdnjs-github@cloudflare.com>
2020-03-27 11:01:21 +0000

    Add simple-icons v1.9.15

---
+++ ajax/libs/simple-icons/1.9.15/nintendo.js
@@ -0,0 +1,1 @@
+module.exports={"title":"Nintendo","hex":"8F8F8F","source":"https://en.wikipedia.org/wiki/Nintendo#/media/File:Nintendo.svg","svg":"<svg role=\"img\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><title>Nintendo icon</title><path d=\"M0 .6h7.1l9.85 15.9V.6H24v22.8h-7.04L7.06 7.5v15.9H0V.6\"/></svg>"};
