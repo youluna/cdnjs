54e44af32bb10da1ccec343c2461d9b1c04ba6d5
robocdnjs <cdnjs-github@cloudflare.com>
2020-03-27 11:19:32 +0000

    Add simple-icons v1.9.28

---
+++ ajax/libs/simple-icons/1.9.28/discover.js
@@ -0,0 +1,1 @@
+module.exports={"title":"Discover","hex":"FF6000","source":"https://www.discovernetwork.com/en-us/business-resources/free-signage-logos","svg":"<svg role=\"img\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><title>Discover icon</title><path d=\"M12 0A12 12 0 1 0 12 24A12 12 0 1 0 12 0Z\"/></svg>"};
