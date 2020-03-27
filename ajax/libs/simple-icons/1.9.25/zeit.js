62e5d3ce8e652a37d0e4809018ae47b4c36358ce
robocdnjs <cdnjs-github@cloudflare.com>
2020-03-27 11:15:07 +0000

    Add simple-icons v1.9.25

---
+++ ajax/libs/simple-icons/1.9.25/zeit.js
@@ -0,0 +1,1 @@
+module.exports={"title":"Zeit","hex":"000000","source":"https://zeit.co/design/brand","svg":"<svg role=\"img\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><title>Zeit icon</title><path d=\"M24 22.525H0l12-21.05 12 21.05z\"/></svg>"};
