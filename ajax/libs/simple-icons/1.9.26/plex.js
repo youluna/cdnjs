3b42938942db1d38deb11cd371f965dbc50784e4
robocdnjs <cdnjs-github@cloudflare.com>
2020-03-27 11:16:34 +0000

    Add simple-icons v1.9.26

---
+++ ajax/libs/simple-icons/1.9.26/plex.js
@@ -0,0 +1,1 @@
+module.exports={"title":"Plex","hex":"E5A00D","source":"http://brand.plex.tv/d/qxmJ3odkK0fj/plex-style-guide","svg":"<svg role=\"img\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\"><title>Plex icon</title><path d=\"M11.643 0H4.68l7.679 12L4.68 24h6.963l7.677-12-7.677-12\"/></svg>"};
