18712024aa4a7ed80cb3dbdd33045ae9ffee9eed
robocdnjs <cdnjs-github@cloudflare.com>
2020-04-08 08:32:57 +0000

    Add icepick v2.0.0

---
+++ ajax/libs/icepick/2.0.0/icepick.js
@@ -0,0 +1,309 @@
+/**
+ * This allows you to work with object hierarchies that have been frozen
+ * with Object.freeze().  "get" operations can use the normal JS syntax,
+ * but operations that modify the data will have to return partial copies of
+ * the structure. The portions of the structure that did not change will
+ * === their previous values.
+ *
+ * Inspired by clojure/mori and Immutable.js
+ */
+
+'use strict'
+
+const i = exports
+
+// we only care about objects or arrays for now
+const weCareAbout = val => val !== null &&
+    (Array.isArray(val) ||
+      // This will skip objects created with `new Foo()`
+      // and objects created with `Object.create(proto)`
+      // The benefit is ignoring DOM elements and event emitters,
+      // which are often circular.
+      isObjectLike(val))
+
+const isObjectLike = val => typeof val === 'object' &&
+    val.constructor === Object &&
+    Object.getPrototypeOf(val) === Object.prototype
+
+const clone = (coll) => {
+  if (Array.isArray(coll)) {
+    return [...coll]
+  } else {
+    return Object.assign({}, coll)
+  }
+}
+
+const freezeIfNeeded = coll => {
+  if (
+      weCareAbout(coll) &&
+      (
+        !Object.isFrozen(coll) &&
+        process.env.NODE_ENV !== 'production'
+      )) {
+    return baseFreeze(coll, [])
+  }
+  return coll
+}
+
+const _freeze = coll => {
+  if (process.env.NODE_ENV === 'production') {
+    return coll
+  }
+  if (typeof coll === 'object') {
+    return Object.freeze(coll)
+  } else {
+    return coll
+  }
+}
+
+const baseFreeze = (coll, prevNodes) => {
+  if (prevNodes.some(node => node === coll)) {
+    throw new Error('object has a reference cycle')
+  }
+
+  Object.freeze(coll)
+  prevNodes.push(coll)
+  Object.keys(coll).forEach((key) => {
+    const prop = coll[key]
+    if (weCareAbout(prop)) {
+      baseFreeze(prop, prevNodes)
+    }
+  })
+  prevNodes.pop()
+
+  return coll
+}
+
+/**
+ * recrursively freeze an object and all its child objects
+ * @param  {Object|Array} coll
+ * @return {Object|Array}
+ */
+exports.freeze = function freeze (coll) {
+  if (process.env.NODE_ENV === 'production') {
+    return coll
+  }
+  return baseFreeze(coll, [])
+}
+
+/**
+ * recursively un-freeze an object, by cloning frozen collections
+ * @param  {[type]} coll [description]
+ * @return {[type]}      [description]
+ */
+exports.thaw = function thaw (coll) {
+  if (weCareAbout(coll) && Object.isFrozen(coll)) {
+    const newColl = clone(coll)
+    Object.keys(newColl).forEach((key) => {
+      newColl[key] = thaw(newColl[key])
+    })
+    return newColl
+  }
+  return coll
+}
+
+/**
+ * set a value on an object or array
+ * @param  {Object|Array}  coll
+ * @param  {String|Number} key   Key or index
+ * @param  {Object}        value
+ * @return {Object|Array}        new object hierarchy with modifications
+ */
+exports.assoc = function assoc (coll, key, value) {
+  if (coll[key] === value) {
+    return _freeze(coll)
+  }
+
+  const newObj = clone(coll)
+
+  newObj[key] = freezeIfNeeded(value)
+
+  return _freeze(newObj)
+}
+exports.set = exports.assoc
+
+/**
+ * un-set a value on an object or array
+ * @param  {Object|Array}  coll
+ * @param  {String|Number} key  Key or Index
+ * @return {Object|Array}       New object or array
+ */
+exports.dissoc = function dissoc (coll, key) {
+  const newObj = clone(coll)
+
+  delete newObj[key]
+
+  return _freeze(newObj)
+}
+exports.unset = exports.dissoc
+
+/**
+ * set a value deep in a hierarchical structure
+ * @param  {Object|Array} coll
+ * @param  {Array}        path    A list of keys to traverse
+ * @param  {Object}       value
+ * @return {Object|Array}       new object hierarchy with modifications
+ */
+exports.assocIn = function assocIn (coll, path, value) {
+  const key0 = path[0]
+  if (path.length === 1) {
+    // simplest case is a 1-element array.  Just a simple assoc.
+    return i.assoc(coll, key0, value)
+  } else {
+    // break the problem down.  Assoc this object with the first key
+    // and the result of assocIn with the rest of the keys
+    return i.assoc(coll, key0, assocIn(coll[key0] || {}, path.slice(1), value))
+  }
+}
+exports.setIn = exports.assocIn
+
+/**
+ * get an object from a hierachy based on an array of keys
+ * @param  {Object|Array} coll
+ * @param  {Array}        path    list of keys
+ * @return {Object}       value, or undefined
+ */
+function baseGet (coll, path) {
+  return (path || []).reduce((curr, key) => {
+    if (!curr) { return }
+    return curr[key]
+  }, coll)
+}
+
+exports.getIn = baseGet
+
+/**
+ * Update a value in a hierarchy
+ * @param  {Object|Array}   coll
+ * @param  {Array}          path     list of keys
+ * @param  {Function} callback The existing value with be passed to this.
+ *                             Return the new value to set
+ * @return {Object|Array}      new object hierarchy with modifications
+ */
+exports.updateIn = function updateIn (coll, path, callback) {
+  const existingVal = baseGet(coll, path)
+  return i.assocIn(coll, path, callback(existingVal))
+};
+
+// generate wrappers for the mutative array methods
+['push', 'unshift', 'pop', 'shift', 'reverse', 'sort']
+.forEach((methodName) => {
+  exports[methodName] = function (arr, val) {
+    const newArr = [...arr]
+
+    newArr[methodName](freezeIfNeeded(val))
+
+    return _freeze(newArr)
+  }
+
+  exports[methodName].displayName = 'icepick.' + methodName
+})
+
+// splice is special because it is variadic
+exports.splice = function splice (arr, ..._args) {
+  const newArr = [...arr]
+  const args = _args.map(freezeIfNeeded)
+
+  newArr.splice.apply(newArr, args)
+
+  return _freeze(newArr)
+}
+
+// slice is non-mutative
+exports.slice = function slice (arr, arg1, arg2) {
+  const newArr = arr.slice(arg1, arg2)
+
+  return _freeze(newArr)
+};
+
+['map', 'filter'].forEach((methodName) => {
+  exports[methodName] = function (fn, arr) {
+    const newArr = arr[methodName](fn)
+
+    return _freeze(newArr)
+  }
+
+  exports[methodName].displayName = 'icepick.' + methodName
+})
+
+exports.extend =
+exports.assign = function assign (obj, ...objs) {
+  const newObj = objs.reduce(singleAssign, obj)
+
+  return _freeze(newObj)
+}
+
+function singleAssign (obj1, obj2) {
+  return Object.keys(obj2).reduce((obj, key) => {
+    return i.assoc(obj, key, obj2[key])
+  }, obj1)
+}
+
+exports.merge = merge
+function merge (target, source, resolver) {
+  if (target == null || source == null) {
+    return target
+  }
+  return Object.keys(source).reduce((obj, key) => {
+    const sourceVal = source[key]
+    const targetVal = obj[key]
+
+    const resolvedSourceVal =
+      resolver ? resolver(targetVal, sourceVal, key) : sourceVal
+
+    if (weCareAbout(sourceVal) && weCareAbout(targetVal)) {
+      // if they are both frozen and reference equal, assume they are deep equal
+      if ((
+            (Object.isFrozen(resolvedSourceVal) &&
+              Object.isFrozen(targetVal)) ||
+            process.env.NODE_ENV === 'production'
+          ) &&
+          resolvedSourceVal === targetVal) {
+        return obj
+      }
+      if (Array.isArray(sourceVal)) {
+        return i.assoc(obj, key, resolvedSourceVal)
+      }
+      // recursively merge pairs of objects
+      return assocIfDifferent(obj, key,
+        merge(targetVal, resolvedSourceVal, resolver))
+    }
+
+    // primitive values, stuff with prototypes
+    return assocIfDifferent(obj, key, resolvedSourceVal)
+  }, target)
+}
+
+function assocIfDifferent (target, key, value) {
+  if (target[key] === value) {
+    return target
+  }
+  return i.assoc(target, key, value)
+}
+
+const chainProto = {
+  value: function value () {
+    return this.val
+  },
+  thru: function thru (fn) {
+    this.val = freezeIfNeeded(fn(this.val))
+    return this
+  }
+}
+
+Object.keys(exports).forEach((methodName) => {
+  chainProto[methodName] = function (...args) {
+    args.unshift(this.val)
+    this.val = exports[methodName].apply(null, args)
+    return this
+  }
+})
+
+exports.chain = function chain (val) {
+  const wrapped = Object.create(chainProto)
+  wrapped.val = val
+  return wrapped
+}
+
+// for testing
+exports._weCareAbout = weCareAbout
---
+++ ajax/libs/icepick/2.0.0/icepick.test.js
@@ -0,0 +1,604 @@
+/* global describe, it, before, after */
+const expect = require('expect.js')
+const i = require('../icepick')
+
+describe('icepick', function () {
+  'use strict'
+
+  describe('freeze', function () {
+    it('should work', function () {
+      const a = i.freeze({asdf: 'foo', zxcv: {asdf: 'bar'}})
+
+      expect(a.asdf).to.equal('foo')
+      expect(Object.isFrozen(a)).to.equal(true)
+
+      expect(function () {
+        a.asdf = 'bar'
+      }).to.throwError()
+
+      expect(function () {
+        a.zxcv.asdf = 'qux'
+      }).to.throwError()
+
+      expect(function () {
+        a.qwer = 'bar'
+      }).to.throwError()
+    })
+
+    it('should not work with cyclical objects', function () {
+      let a = {}
+      a.a = a
+
+      expect(i.freeze).withArgs(a).to.throwError()
+
+      a = {b: {}}
+      a.b.a = a
+      expect(i.freeze).withArgs(a).to.throwError()
+    })
+  })
+
+  describe('thaw', function () {
+    function Foo () {}
+
+    it('should thaw objects', function () {
+      const o = i.freeze({
+        a: {},
+        b: 1,
+        c: new Foo(),
+        d: [{e: 1}]
+      })
+
+      const thawed = i.thaw(o)
+
+      expect(thawed).to.eql(o)
+      expect(Object.isFrozen(thawed)).to.be(false)
+      expect(Object.isFrozen(thawed.a)).to.be(false)
+      expect(o.a).to.not.equal(thawed.a)
+      expect(o.d).to.not.equal(thawed.d)
+      expect(o.d[0]).to.not.equal(thawed.d[0])
+      expect(o.c).to.equal(thawed.c)
+    })
+  })
+
+  describe('assoc', function () {
+    it('should work with objects', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      let result = i.assoc(o, 'b', 4)
+
+      expect(result).to.eql({a: 1, b: 4, c: 3})
+
+      result = i.assoc(o, 'd', 4)
+      expect(result).to.eql({a: 1, b: 2, c: 3, d: 4})
+    })
+
+    it('should freeze objects you assoc', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      const result = i.assoc(o, 'b', {d: 5})
+
+      expect(result).to.eql({a: 1, b: {d: 5}, c: 3})
+
+      expect(Object.isFrozen(result.b)).to.be.ok()
+    })
+
+    it('should work with arrays', function () {
+      const a = i.freeze([1, 2, 3])
+      let result = i.assoc(a, 1, 4)
+
+      expect(result).to.eql([1, 4, 3])
+
+      result = i.assoc(a, '1', 4)
+      expect(result).to.eql([1, 4, 3])
+
+      result = i.assoc(a, 3, 4)
+      expect(result).to.eql([1, 2, 3, 4])
+    })
+
+    it('should freeze arrays you assoc', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      const result = i.assoc(o, 'b', [1, 2])
+
+      expect(result).to.eql({a: 1, b: [1, 2], c: 3})
+
+      expect(Object.isFrozen(result.b)).to.be.ok()
+    })
+
+    it('should return a frozen copy', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      const result = i.assoc(o, 'b', 4)
+
+      expect(result).to.not.equal(o)
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('should not modify child objects', function () {
+      const o = i.freeze({a: 1, b: 2, c: {a: 4}})
+      const result = i.assoc(o, 'b', 4)
+
+      expect(result.c).to.equal(o.c)
+    })
+
+    it('should keep references the same if nothing changes', function () {
+      const o = i.freeze({a: 1})
+      const result = i.assoc(o, 'a', 1)
+      expect(result).to.equal(o)
+    })
+
+    it('should be aliased as set', function () {
+      expect(i.set).to.equal(i.assoc)
+    })
+  })
+
+  describe('dissoc', function () {
+    it('should work with objecs', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      const result = i.dissoc(o, 'b')
+
+      expect(result).to.eql({a: 1, c: 3})
+    })
+
+    it('should work with arrays (poorly)', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.dissoc(a, 1)
+
+      // expect(result).to.eql([1, , 3])
+      expect(Object.keys(result)).to.eql([0, 2])
+      expect(result[0]).to.equal(1)
+      expect(result[1]).to.equal(undefined)
+      expect(result[2]).to.equal(3)
+    })
+
+    it('should be aliased as unset', function () {
+      expect(i.unset).to.equal(i.dissoc)
+    })
+  })
+
+  describe('assocIn', function () {
+    it('should work recursively', function () {
+      const o = i.freeze({a: 1, b: 2, c: {a: 4}})
+      const result = i.assocIn(o, ['c', 'a'], 5)
+
+      expect(result).to.eql({a: 1, b: 2, c: {a: 5}})
+    })
+
+    it('should work recursively (deeper)', function () {
+      const o = i.freeze({
+        a: 1,
+        b: {a: 2},
+        c: [
+          {
+            a: 3,
+            b: 4
+          },
+          {a: 4}
+        ]
+      })
+      const result = i.assocIn(o, ['c', 0, 'a'], 8)
+
+      expect(result.c[0].a).to.equal(8)
+      expect(result).to.not.equal(o)
+      expect(result.b).to.equal(o.b)
+      expect(result.c).to.not.equal(o.c)
+      expect(result.c[0]).to.not.equal(o.c[0])
+      expect(result.c[0].b).to.equal(o.c[0].b)
+      expect(result.c[1]).to.equal(o.c[1])
+    })
+
+    it("should create collections if they don't exist", function () {
+      const result = i.assocIn({}, ['a', 'b', 'c'], 1)
+      expect(result).to.eql({a: {b: {c: 1}}})
+    })
+
+    it('should be aliased as setIn', function () {
+      expect(i.setIn).to.equal(i.assocIn)
+    })
+
+    it('should keep references the same if nothing changes', function () {
+      const o = i.freeze({a: {b: 1}})
+      const result = i.assocIn(o, ['a', 'b'], 1)
+      expect(result).to.equal(o)
+    })
+  })
+
+  describe('getIn', function () {
+    it('should work', function () {
+      const o = i.freeze({
+        a: 0,
+        b: {a: 2},
+        c: [
+            {a: 3, b: 4},
+            {a: 4}
+        ]
+      })
+      expect(i.getIn(o, ['c', 0, 'b'])).to.equal(4)
+      expect(i.getIn(o, ['a'])).to.equal(0)
+    })
+
+    it('should work without a path', function () {
+      const o = i.freeze({a: {b: 1}})
+      expect(i.getIn(o)).to.equal(o)
+    })
+
+    it('should return undefined for a non-existant path', function () {
+      const o = i.freeze({
+        a: 1,
+        b: {a: 2},
+        c: [
+          {a: 3, b: 4},
+          {a: 4}
+        ]
+      })
+
+      expect(i.getIn(o, ['q'])).to.equal(undefined)
+      expect(i.getIn(o, ['a', 's', 'd'])).to.equal(undefined)
+    })
+
+    it('should return undefined for a non-existant path (null)', function () {
+      const o = i.freeze({
+        a: null
+      })
+
+      expect(i.getIn(o, ['a', 'b'])).to.equal(undefined)
+    })
+  })
+
+  describe('updateIn', function () {
+    it('should work', function () {
+      const o = i.freeze({a: 1, b: 2, c: {a: 4}})
+      const result = i.updateIn(o, ['c', 'a'], function (num) {
+        return num * 2
+      })
+
+      expect(result).to.eql({a: 1, b: 2, c: {a: 8}})
+    })
+
+    it("should create collections if they don't exist", function () {
+      const result = i.updateIn({}, ['a', 1, 'c'], function (val) {
+        expect(val).to.be(undefined)
+        return 1
+      })
+      expect(result).to.eql({a: {'1': {c: 1}}})
+    })
+
+    it('should keep references the same if nothing changes', function () {
+      const o = i.freeze({a: 1})
+      const result = i.updateIn(o, ['a', 'b'], function (v) { return v })
+      expect(result).to.equal(o)
+    })
+  })
+
+  describe('Array methods', function () {
+    it('push', function () {
+      const a = i.freeze([1, 2])
+      const result = i.push(a, 3)
+
+      expect(result).to.eql([1, 2, 3])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('push (with object)', function () {
+      const a = i.freeze([1, 2])
+      const result = i.push(a, {b: 1})
+
+      expect(result).to.eql([1, 2, {b: 1}])
+      expect(Object.isFrozen(result)).to.be.ok()
+      expect(Object.isFrozen(result[2])).to.be.ok()
+    })
+
+    it('unshift', function () {
+      const a = i.freeze([1, 2])
+      const result = i.unshift(a, 3)
+
+      expect(result).to.eql([3, 1, 2])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('unshift (with object)', function () {
+      const a = i.freeze([1, 2])
+      const result = i.unshift(a, [0])
+
+      expect(result).to.eql([[0], 1, 2])
+      expect(Object.isFrozen(result)).to.be.ok()
+      expect(Object.isFrozen(result[0])).to.be.ok()
+    })
+
+    it('pop', function () {
+      const a = i.freeze([1, 2])
+      const result = i.pop(a)
+
+      expect(result).to.eql([1])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('shift', function () {
+      const a = i.freeze([1, 2])
+      const result = i.shift(a)
+
+      expect(result).to.eql([2])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('reverse', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.reverse(a)
+
+      expect(result).to.eql([3, 2, 1])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('sort', function () {
+      const a = i.freeze([4, 1, 2, 3])
+      const result = i.sort(a)
+
+      expect(result).to.eql([1, 2, 3, 4])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('splice', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.splice(a, 1, 1, 4)
+
+      expect(result).to.eql([1, 4, 3])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('splice (with object)', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.splice(a, 1, 1, {b: 1}, {b: 2})
+
+      expect(result).to.eql([1, {b: 1}, {b: 2}, 3])
+      expect(Object.isFrozen(result)).to.be.ok()
+      expect(Object.isFrozen(result[1])).to.be.ok()
+      expect(Object.isFrozen(result[2])).to.be.ok()
+    })
+
+    it('slice', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.slice(a, 1, 2)
+
+      expect(result).to.eql([2])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('map', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.map(function (v) { return v * 2 }, a)
+
+      expect(result).to.eql([2, 4, 6])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+
+    it('filter', function () {
+      const a = i.freeze([1, 2, 3])
+      const result = i.filter(function (v) { return v % 2 }, a)
+
+      expect(result).to.eql([1, 3])
+      expect(Object.isFrozen(result)).to.be.ok()
+    })
+  })
+
+  describe('assign', function () {
+    it('should work', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      let result = i.assign(o, {'b': 3, 'c': 4})
+      expect(result).to.eql({a: 1, b: 3, c: 4})
+      expect(result).to.not.equal(o)
+      result = i.assign(o, {'d': 4})
+      expect(result).to.eql({a: 1, b: 2, c: 3, d: 4})
+    })
+
+    it('should work with multiple args', function () {
+      const o = i.freeze({a: 1, b: 2, c: 3})
+      const result = i.assign(o, {'b': 3, 'c': 4}, {'d': 4})
+      expect(result).to.eql({a: 1, b: 3, c: 4, d: 4})
+    })
+
+    it('should keep references the same if nothing changes', function () {
+      const o = i.freeze({a: 1})
+      const result = i.assign(o, {a: 1})
+      expect(result).to.equal(o)
+    })
+  })
+
+  describe('merge', function () {
+    it('should merge nested objects', function () {
+      const o1 = i.freeze({a: 1, b: {c: 1, d: 1}})
+      const o2 = i.freeze({a: 1, b: {c: 2}, e: 2})
+
+      const result = i.merge(o1, o2)
+      expect(result).to.eql({a: 1, b: {c: 2, d: 1}, e: 2})
+    })
+
+    it('should replace arrays', function () {
+      const o1 = i.freeze({a: 1, b: {c: [1, 1]}, d: 1})
+      const o2 = i.freeze({a: 2, b: {c: [2]}})
+
+      const result = i.merge(o1, o2)
+      expect(result).to.eql({a: 2, b: {c: [2]}, d: 1})
+    })
+
+    it('should overwrite with nulls', function () {
+      const o1 = i.freeze({a: 1, b: {c: [1, 1]}})
+      const o2 = i.freeze({a: 2, b: {c: null}})
+
+      const result = i.merge(o1, o2)
+      expect(result).to.eql({a: 2, b: {c: null}})
+    })
+
+    it('should overwrite primitives with objects', function () {
+      const o1 = i.freeze({a: 1, b: 1})
+      const o2 = i.freeze({a: 2, b: {c: 2}})
+
+      const result = i.merge(o1, o2)
+      expect(result).to.eql({a: 2, b: {c: 2}})
+    })
+
+    it('should overwrite objects with primitives', function () {
+      const o1 = i.freeze({a: 1, b: {c: 2}})
+      const o2 = i.freeze({a: 1, b: 2})
+
+      const result = i.merge(o1, o2)
+      expect(result).to.eql({a: 1, b: 2})
+    })
+
+    it('should keep references the same if nothing changes', function () {
+      const o1 = i.freeze({a: 1, b: {c: 1, d: 1, e: [1]}})
+      const o2 = i.freeze({a: 1, b: {c: 1, d: 1, e: o1.b.e}})
+      const result = i.merge(o1, o2)
+      expect(result).to.equal(o1)
+      expect(result.b).to.equal(o1.b)
+    })
+
+    it('should handle undefined parameters', function () {
+      expect(i.merge({}, undefined)).to.eql({})
+      expect(i.merge(undefined, {})).to.eql(undefined)
+    })
+
+    describe('custom associator', function () {
+      it('should use the custom associator', function () {
+        const o1 = i.freeze({a: 1, b: {c: [1, 1]}, d: 1})
+        const o2 = i.freeze({a: 2, b: {c: [2]}})
+
+        function resolver (targetVal, sourceVal) {
+          if (Array.isArray(targetVal) && sourceVal) {
+            return targetVal.concat(sourceVal)
+          } else {
+            return sourceVal
+          }
+        }
+
+        const result = i.merge(o1, o2, resolver)
+        expect(result).to.eql({a: 2, b: {c: [1, 1, 2]}, d: 1})
+      })
+    })
+  })
+})
+
+describe('chain', function () {
+  it('should wrap and unwrap a value', function () {
+    const a = [1, 2, 3]
+    const result = i.chain(a).value()
+    expect(result).to.eql(a)
+  })
+
+  it('should work with a simple operation', function () {
+    const a = [1, 2, 3]
+    const result = i.chain(a)
+      .assoc(1, 4)
+      .value()
+    expect(result).to.eql([1, 4, 3])
+    expect(result).to.not.equal(a)
+    expect(Object.isFrozen(result)).to.be.ok()
+  })
+
+  it('should work with multiple operations', function () {
+    const a = [1, 2, 3]
+    const result = i.chain(a)
+      .assoc(1, 4)
+      .reverse()
+      .pop()
+      .push(5)
+      .value()
+    expect(result).to.eql([3, 4, 5])
+    expect(result).to.not.equal(a)
+    expect(Object.isFrozen(result)).to.be.ok()
+  })
+
+  it('should work with multiple operations (more complicated)', function () {
+    const o = {
+      a: [1, 2, 3],
+      b: {c: 1},
+      d: 4
+    }
+    const result = i.chain(o)
+      .assocIn(['a', 2], 4)
+      .merge({b: {c: 2, c2: 3}})
+      .assoc('e', 2)
+      .dissoc('d')
+      .value()
+    expect(result).to.eql({
+      a: [1, 2, 4],
+      b: {c: 2, c2: 3},
+      e: 2
+    })
+    expect(result).to.not.equal(o)
+    expect(Object.isFrozen(result)).to.be.ok()
+  })
+
+  it('should have a thru method', function () {
+    const o = [1, 2]
+    const result = i.chain(o)
+      .push(3)
+      .thru(function (val) {
+        return [0].concat(val)
+      })
+      .value()
+    expect(Object.isFrozen(result)).to.be.ok()
+    expect(result).to.eql([0, 1, 2, 3])
+  })
+})
+
+describe('production mode', function () {
+  let oldEnv
+  before(function () {
+    oldEnv = process.env.NODE_ENV
+    process.env.NODE_ENV = 'production'
+  })
+
+  after(function () {
+    process.env.NODE_ENV = oldEnv
+  })
+
+  it('should not freeze objects', function () {
+    const result = i.freeze({})
+    expect(Object.isFrozen(result)).to.be(false)
+  })
+
+  it("should not freeze objects that are assoc'd", function () {
+    const result = i.assoc({}, 'a', {})
+    expect(Object.isFrozen(result)).to.be(false)
+    expect(Object.isFrozen(result.a)).to.be(false)
+  })
+
+  it('merge should keep references the same if nothing changes', function () {
+    const o1 = i.freeze({a: 1, b: {c: 1, d: 1, e: [1]}})
+    const o2 = i.freeze({a: 1, b: {c: 1, d: 1, e: o1.b.e}})
+    const result = i.merge(o1, o2)
+    expect(result).to.equal(o1)
+    expect(result.b).to.equal(o1.b)
+  })
+})
+
+describe('internals', function () {
+  describe('_weCareAbout', function () {
+    function Foo () {}
+
+    it('should care about objects', function () {
+      expect(i._weCareAbout({})).to.equal(true)
+    })
+    it('should care about arrays', function () {
+      expect(i._weCareAbout([])).to.equal(true)
+    })
+    it('should not care about dates', function () {
+      expect(i._weCareAbout(new Date())).to.equal(false)
+    })
+    it('should not care about null', function () {
+      expect(i._weCareAbout(null)).to.equal(false)
+    })
+    it('should not care about undefined', function () {
+      expect(i._weCareAbout(undefined)).to.equal(false)
+    })
+    it('should not care about class instances', function () {
+      expect(i._weCareAbout(new Foo())).to.equal(false)
+    })
+    it('should not care about objects created with Object.create()',
+    function () {
+      expect(i._weCareAbout(Object.create(Foo.prototype))).to.equal(false)
+    })
+    it('should not care about objects created with Object.create({})',
+    function () {
+      expect(i._weCareAbout(Object.create({
+        foo: function () {}
+      }))).to.equal(false)
+    })
+  })
+})
